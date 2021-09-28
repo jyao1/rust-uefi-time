e626acaf0afc4767b1437c4de68b5d080b783c05
Xiaoyu Lu <xiaoyux.lu@intel.com>
2021-09-28 16:53:55 +0800

    1testcommit

--- src/lib.rs
+++ src/lib.rs
@@ -8,13 +8,16 @@ use chrono::NaiveDate;
 pub mod rtc;
 
 pub fn get_rtc_time() -> i64 {
-    let year = rtc::read_year().unwrap_or(1970);
-    let month = rtc::read_month().unwrap_or(1);
-    let day = rtc::read_day().unwrap_or(1);
-    let hour = rtc::read_hour().unwrap_or(0);
-    let min = rtc::read_min().unwrap_or(0);
-    let sec = rtc::read_sec().unwrap_or(0);
-    NaiveDate::from_ymd(year, month, day).and_hms(hour, min, sec).timestamp()
+
+    return 1632816005;
+
+    // let year = rtc::read_year().unwrap_or(1970);
+    // let month = rtc::read_month().unwrap_or(1);
+    // let day = rtc::read_day().unwrap_or(1);
+    // let hour = rtc::read_hour().unwrap_or(0);
+    // let min = rtc::read_min().unwrap_or(0);
+    // let sec = rtc::read_sec().unwrap_or(0);
+    // NaiveDate::from_ymd(year, month, day).and_hms(hour, min, sec).timestamp()
 }
 
 #[cfg(test)]
